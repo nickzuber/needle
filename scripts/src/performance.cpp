1238fee3f3f10794806261ca2490ffe4f97c72b9
nickzuber <nickzoober@gmail.com>
2016-04-04 17:32:21 -0400

    working on setting up node addon (currently an issue with bindings path finding)

---
+++ scripts/src/performance.cpp
@@ -0,0 +1,27 @@
+#include <nan.h>
+
+void Pow(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+
+	if (info.Length() < 2) {
+		Nan::ThrowTypeError("Wrong number of arguments");
+		return;
+	}
+
+	if (!info[0]->IsNumber() || !info[1]->IsNumber()) {
+		Nan::ThrowTypeError("Both arguments should be numbers");
+		return;
+	}
+
+	double arg0 = info[0]->NumberValue();
+	double arg1 = info[1]->NumberValue();
+	v8::Local<v8::Number> num = Nan::New(pow(arg0, arg1));
+
+	info.GetReturnValue().Set(num);
+}
+
+void Init(v8::Local<v8::Object> exports) {
+	exports->Set(Nan::New("pow").ToLocalChecked(),
+		Nan::New<v8::FunctionTemplate>(Pow)->GetFunction());
+}
+
+NODE_MODULE(addon, Init)
